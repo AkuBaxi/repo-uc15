6ac8dd0d15b00ad589245bed81d07fb5c327308d
Haruu <133592073+AkuBaxi@users.noreply.github.com>
2024-09-12 10:10:33 -0300

    Add pasta
    Atividade dia 04 Operadores Aritméticos

---
+++ javascript-essentials/dia_04/exercicio_pratico_operadores_aritmeticos.js
@@ -0,0 +1,46 @@
+// Crie um script que:
+
+// Declare duas variáveis numéricas
+// Realize todas as operações aritméticas básicas com essas variáveis
+// Use operadores de incremento e decremento
+// Aplique operadores de atribuição composta
+// Imprima todos os resultados no console
+// Exemplo de solução:
+
+// let a = 10;
+// let b = 3;
+
+// console.log("Adição:", a + b);
+// console.log("Subtração:", a - b);
+// console.log("Multiplicação:", a * b);
+// console.log("Divisão:", a / b);
+// console.log("Módulo:", a % b);
+// console.log("Exponenciação:", a ** b);
+
+// console.log("Incremento de a:", ++a);
+// console.log("Decremento de b:", --b);
+
+// a += b;
+// console.log("a += b:", a);
+
+// a *= b;
+// console.log("a *= b:", a);
+
+let x = 12;
+let y = 5;
+
+console.log("Adição:", x + y)             // Adição
+console.log("Subtração:", x - y)          // Subtração
+console.log("Multiplicação:", x * y)      // Multiplicação
+console.log("Divisão:", x / y)            // Divisão
+console.log("Módulo:", x % y)             // Módulo
+console.log("Exponenciação:",  x ** y)    // Exponenciação
+
+console.log("Incremento de a:", ++x)       // Incremento de a 
+console.log("Decremento de b:", --y)       // Decremento de b 
+
+x += y
+console.log("x += y:", x)               
+
+x *= y
+console.log("a *= b:", x)             
